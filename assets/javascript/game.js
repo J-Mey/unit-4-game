05dc48cbc547534610bb86febf12827eebfb62e2
J <j.mey.r8@gmail.com>
2019-05-05 23:16:18 -0400

    finished layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+//number guess is between 19-120
+// crystal number are between 1-12
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
